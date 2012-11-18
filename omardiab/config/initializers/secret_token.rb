9548def3d933a7add98e39eddcc69e3acc80e983
Omar Diab <o.s.diab@gmail.com>
2012-11-17 22:08:16 -0800

    added starter rails project

---
+++ omardiab/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omardiab::Application.config.secret_token = '2be80697c628444a8f9c9e587ab4856c8555f66fb67cbfe2a25f917c57d0790dfd2cceebb3d9f71f358153d9792979ad80a2f60a4bae666780aea0e0af48835a'
