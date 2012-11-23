5855ea07f140d92a525f476917c42d95de6c8482
Omar Diab <o.s.diab@gmail.com>
2012-11-22 23:45:17 -0800

    made home route properly

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,6 @@ Omardiab::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
   root to: 'home#index', via: 'get'
+  get 'home' => 'home#index'
   get 'about' => 'about#index'
 end
