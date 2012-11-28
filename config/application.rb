fda484d5b6766e9b447feee184fe5075576614a1
Omar Diab <o.s.diab@gmail.com>
2012-11-27 23:14:26 -0800

    moved sections to yaml rather than rails config

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module Omardiab
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.sections = ['home', 'projects', 'experience', 'links', 'contact']
   end
 end
